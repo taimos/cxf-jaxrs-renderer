5d4ad54d10081b89c6c812c03641d6cf9cab3706
hoegertn <thorsten.hoeger@hoegernet.com>
2013-02-24 15:19:40 +0100

    license

--- src/main/java/de/taimos/cxf_renderer/velocity/VelocityBodyWriter.java
+++ src/main/java/de/taimos/cxf_renderer/velocity/VelocityBodyWriter.java
@@ -1,7 +1,24 @@
-/**
- * 
- */
 package de.taimos.cxf_renderer.velocity;
+
+/*
+ * #%L
+ * MVC Renderer for JAX-RS with CXF
+ * %%
+ * Copyright (C) 2012 - 2013 Taimos GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -27,12 +44,6 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import de.taimos.cxf_renderer.model.AbstractViewBodyWriter;
 import de.taimos.cxf_renderer.model.ViewModel;
 
-/**
- * @author thoeger
- * 
- *         Copyright 2012, Taimos GmbH
- * 
- */
 public abstract class VelocityBodyWriter extends AbstractViewBodyWriter {
 
 	static {
